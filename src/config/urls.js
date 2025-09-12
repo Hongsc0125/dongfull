d3a1aec2e3d270a43555ba20376c1a2f131611d5
Hongsc0125 <hsc0125@knou.ac.kr>
2025-09-12 13:49:05 +0900

    fix: Discord 봇 썸네일용 왕관 이미지 URL 수정
    - SVG 대신 PNG 이미지 사용 (Discord에서 더 잘 지원)
    - 안정적인 외부 이미지 URL로 변경
    - Discord embed thumbnail 정상 표시 보장

--- src/config/urls.js
+++ src/config/urls.js
@@ -3,5 +3,5 @@ export const STATIC_BASE_URL = process.env.STATIC_BASE_URL || 'https://evt-board
 
 export const STATIC_URLS = {
   ALARM: `${STATIC_BASE_URL}/api/static/alarm.svg`,
-  KING: `${STATIC_BASE_URL}/api/static/king.svg`
+  KING: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/crown_1f451.png'
 };
